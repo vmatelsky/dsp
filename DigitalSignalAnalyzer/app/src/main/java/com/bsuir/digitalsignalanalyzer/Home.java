3d3b06e9c9f60e41006fb2391b283d526673d263
Matelsky, Vladislav <VMatelsky@qulix.com>
2014-11-01 14:48:14 +0300

    add examples of signals and display them in list view

--- DigitalSignalAnalyzer/app/src/main/java/com/bsuir/digitalsignalanalyzer/Home.java
+++ DigitalSignalAnalyzer/app/src/main/java/com/bsuir/digitalsignalanalyzer/Home.java
@@ -1,17 +1,17 @@
 package com.bsuir.digitalsignalanalyzer;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ItemClick;
 import org.androidannotations.annotations.ViewById;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -22,38 +22,22 @@ public class Home extends Activity {
     ListView _signalsList;
 
     @AfterViews
-    void initViews() {
-        List<String> your_array_list = new ArrayList<String>();
-        your_array_list.add("foo");
-        your_array_list.add("bar");
+    void initSignalsList() {
+        try {
+            List<String> signals = Arrays.asList(getAssets().list("Prim_Sign"));
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    signals );
 
-        // This is the array adapter, it takes the context of the activity as a
-        // first parameter, the type of list view as a second parameter and your
-        // array as a third parameter.
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                your_array_list );
-
-        _signalsList.setAdapter(arrayAdapter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+            _signalsList.setAdapter(arrayAdapter);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    @ItemClick(R.id.signals_list)
+    void listItemClicked(String food) {
+        Toast.makeText(this, "click: " + food, Toast.LENGTH_SHORT).show();
     }
 }
